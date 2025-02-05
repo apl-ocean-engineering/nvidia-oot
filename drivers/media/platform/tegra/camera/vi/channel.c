2ed8170c048da62b4f8979166b05f1b8713cc708
Dennis Langenkamp <dennis.langenkamp@alliedvision.com>
2025-02-05 11:02:22 +0000

    Fixed UNISDK-2858 Vimba X locks always all cameras if one is opened
    The root cause for the issue is that all cameras / video devices share the same v4l2 priority instance, because all use the same v4l2_dev as parent and by default the v4l2 priority instance is inherited from the v4l2_dev. This now fixed by creating a v4l2 priority instance for each video device on initialization.

--- drivers/media/platform/tegra/camera/vi/channel.c
+++ drivers/media/platform/tegra/camera/vi/channel.c
@@ -2572,12 +2572,20 @@ int tegra_channel_init_video(struct tegra_channel *chan)
 	}
 
 	chan->video = video_device_alloc();
+	
+	chan->video->prio = kzalloc(sizeof(*chan->video->prio), GFP_KERNEL);
+	if (!chan->video->prio) {
+		video_device_release(chan->video);
+		dev_err(&chan->video->dev, "failed allocate prio\n");
+		return -ENOMEM;
+	}
 
 	/* Initialize the media entity... */
 	chan->pad.flags = MEDIA_PAD_FL_SINK;
 	ret = tegra_media_entity_init(&chan->video->entity, 1,
 					&chan->pad, false, false);
 	if (ret < 0) {
+		kfree(chan->video->prio);
 		video_device_release(chan->video);
 		dev_err(&chan->video->dev, "failed to init video entity\n");
 		return ret;
@@ -2616,6 +2624,7 @@ int tegra_channel_init_video(struct tegra_channel *chan)
 	return ret;
 
 ctrl_init_error:
+	kfree(chan->video->prio);
 	video_device_release(chan->video);
 	media_entity_cleanup(&chan->video->entity);
 	v4l2_ctrl_handler_free(&chan->ctrl_handler);
@@ -2732,6 +2741,7 @@ int tegra_channel_cleanup_video(struct tegra_channel *chan)
 {
 	v4l2_ctrl_handler_free(&chan->ctrl_handler);
 	media_entity_cleanup(&chan->video->entity);
+	kfree(chan->video->prio);
 	video_device_release(chan->video);
 	return 0;
 }
